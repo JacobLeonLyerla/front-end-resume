9bb9f1fa1a6955f0a485fb8bd61af1ca1cbcfd84
Jacob Leon Lyerla <umasour@yahoo.com>
2018-06-03 22:56:26 -0700

    set up state for form

--- src/LeftComponent/contactForm.js
+++ src/LeftComponent/contactForm.js
@@ -1,40 +1,70 @@
 import React, { Component } from "react";
-import { Card, CardBody,Label,Input,
-     Button } from 'reactstrap';
-class ContactForm extends Component{
+import axios from "axios";
+import { Card, CardBody, Label, Input, Button } from "reactstrap";
+class ContactForm extends Component {
+  state = {
+    email: "",
+    phone: "",
+    company: "",
+    message: ""
+  };
+  sendMessage = event => {
+    const message = {};
+    message.email = this.state.message;
+    message.phone = this.state.phone;
+    message.company = this.state.company;
+    message.message = this.state.message;
 
-render(){
-    return(
-        <Card className="contactCard-styles">
-      
+    axios
+      .post("https://jacob-lyerla-resume.herokuapp.com/messages", message)
+      .then(respone => {
+        console.log("yata");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+      <Card className="contactCard-styles">
         <CardBody>
-            <Label className="contactLabel-styles">Email</Label>
-        <Input
+          <Label className="contactLabel-styles">Email</Label>
+          <Input
+            onChange={this.handleInputChange}
             placeholder="Email..."
+            value={this.state.email}
             name="email"
-            />
-             <Label className="contactLabel-styles">Phone</Label>
-            <Input
+          />
+          <Label className="contactLabel-styles">Phone</Label>
+          <Input
+            onChange={this.handleInputChange}
             placeholder="Phone..."
-            name="Phone"
-            />
-             <Label className="contactLabel-styles">Company</Label>
-               <Input
+            value={this.state.phone}
+            name="phone"
+          />
+          <Label className="contactLabel-styles">Company</Label>
+          <Input
+            onChange={this.handleInputChange}
             placeholder="Company..."
-            name="Company"
-            />
-             <Label className="contactLabel-styles">Leave a message here</Label>
-<Input
-className="message-styles"
-  type="textarea" 
-
-  placeholder="Message..."
-  name="Message"
-/>
-          <Button>Contact Me</Button>
+            value={this.state.company}
+            name="company"
+          />
+          <Label className="contactLabel-styles">Leave a message here</Label>
+          <Input
+            onChange={this.handleInputChange}
+            className="message-styles"
+            value={this.state.message}
+            type="textarea"
+            placeholder="Message..."
+            name="message"
+          />
+          <Button>Send Message</Button>
         </CardBody>
       </Card>
-    )
-}
+    );
+  }
 }
 export default ContactForm;
