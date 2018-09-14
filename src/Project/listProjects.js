e2021b88be90fe1b27d8945782391108b76f7dbf
Jacob Leon Lyerla <umasour@yahoo.com>
2018-09-13 22:09:25 -0700

    added in a list under my project view, to the links, so i dont have to render them inside of the card

--- src/Project/listProjects.js
+++ src/Project/listProjects.js
@@ -4,12 +4,10 @@ import {
   Card,
   Row,
   Col,
-  CardText,
-  CardTitle,
   CardBody,
-  CardImg,
-  CardLink,
-  Button
+  ListGroup,
+  ListGroupItem,
+  CardLink
 } from "reactstrap";
 
 // import Link from react-router-dom
@@ -17,53 +15,57 @@ import { Link } from "react-router-dom";
 // set up statless component
 
 const Project = props => {
-console.log(props.gif)
+  console.log(props.gif);
   return (
     <Fragment>
       <Col md="8" lg="8" className="progress-styles">
         {/* map over our return for each object in our array */}
         <Row>
           {/* {props.project.map(project => ( */}
-            <Col sm="12" md="12" lg="6" xl="12" className="card-styles">
-              {/* set up a Link for the card */}
+          <Col sm="12" md="12" lg="6" xl="12" className="card-styles">
+            {/* set up a Link for the card */}
+            <Card className={`card${props.index + 1} ${props.gif} `} id="test">
+              <i
+                onMouseLeave={() => props.unToggle("static")}
+                onMouseEnter={() => props.toggle("gif")}
+                id="test2"
+                className="fas fa-play playgif"
+              />
+              <CardBody>
+                <div />
 
-              <Card className={`card${props.index +1} ${props.gif} `} id="test">
-              <i onMouseLeave={()=>props.unToggle("static")} onMouseEnter={()=>props.toggle("gif")}id="test2" className="fas fa-play playgif"></i>
-                <CardBody>
-                 
+                <div className="projectContainer-styles">
+                  <div>
+                    <i
+                      onClick={() => props.incresseIndex("Left")}
+                      className="fas fa-arrow-left fa-2x"
+                    />
+                  </div>
+                  <div>
+                    <i
+                      onClick={() => props.incresseIndex("Right")}
+                      className="fas fa-arrow-right fa-2x"
+                    />
+                  </div>
+                </div>
+              </CardBody>
+            </Card>
+            <ListGroup>
+              <div className="links">
+                <a className="link-styles" href={props.project.webpage}>
+                  {" "}
+                  <ListGroupItem>{props.project.webpage}</ListGroupItem>
+                </a>
+                <a className="link-styles" href={props.project.youtube} >{" "}
                
-<div>
-  
-                  
-                 </div>
-                
-            
-               <div className="projectContainer-styles"><div><i onClick={()=>props.incresseIndex("Left")} className="fas fa-arrow-left fa-2x"></i>
-
-</div><div><i onClick={()=>props.incresseIndex("Right")} className="fas fa-arrow-right fa-2x"></i>
-
-</div></div>
-
-
-
-                    
-                </CardBody> 
-
-                <CardLink href={props.project.youtube}>{props.project.youtube}</CardLink>
-<CardLink href={props.project.github}>
-                  {props.project.github}
-                 </CardLink> 
-
-                <Link
-                  to={`/projects/${props.project._id}`}
-                  style={{ textDecoration: "none" }}
-                >
-                
-                  <Button>View Card</Button>
-                </Link>
-              </Card>
-            </Col>
-          {/* ))} */}
+                  <ListGroupItem>{props.project.youtube}</ListGroupItem>
+             </a >
+                <a className="link-styles" href={props.project.github}>
+                  <ListGroupItem>{props.project.github}</ListGroupItem>
+                </a>
+              </div>
+            </ListGroup>{" "}
+          </Col>
         </Row>
       </Col>
     </Fragment>
