670ced9e0692fe9e01c0d142affadfc39c125bf3
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-14 10:35:44 -0700

    fixed typos

--- src/MiddleComponent/MiddleHeader.js
+++ src/MiddleComponent/MiddleHeader.js
@@ -18,13 +18,13 @@ const Header = () => {
         <CardText>
        <p> I studied computer science in college, learning starting with C# applications than learning C++, mostly learning the fundamental skills of problem solving, where the coding language is just a tool we use to solve problems. 
 </p><p>
-I have moved into learning JavaScript over the last year, and i feel very confident with the language at this point, i joined a hands on program for learning computer science in depth, i commit to doing daily pair programming and i enjoy working inside of a team structure.
+I have moved into learning JavaScript over the last year, and I feel very confident with the language at this point, I joined a hands on program for learning computer science in depth, I commit to doing daily pair programming and I enjoy working inside of a team structure.
 </p><p>
-Inside of this program i have learned new frameworks and skills, and ways to think about problems, i find it really good working with people with all different ways of solving problems, because it allows me to rethink how i break down and build out my own coding solutions, i feel like i am currently growing a lot in this manner.
+Inside of this program I have learned new frameworks and skills, and ways to think about problems, I find it really good working with people with all different ways of solving problems, because it allows me to rethink how I break down and build out my own coding solutions, I feel like I am currently growing a lot in this manner.
 </p><p>
-Moving forward, i am interested in working for a company that values teamwork and communication, working for a company that would allow me to grow as a developer, with a team that pushes and guides me.
+Moving forward, I am interested in working for a company that values teamwork and communication, working for a company that would allow me to grow as a developer, with a team that pushes and guides me.
 </p><p>
-I really enjoy being a developer, and i am very excited to work with an awesome new team, talking about coding and solving problems with a team is a wonderful and growing practice.
+I really enjoy being a developer, and I am very excited to work with an awesome new team, talking about coding and solving problems with a team is a wonderful and growing practice.
 </p>
 Thank you for reading.
 
