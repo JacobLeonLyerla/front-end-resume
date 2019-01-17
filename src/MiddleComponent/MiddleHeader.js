125fc03039e6121134a5504cd69f55a4e2010593
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-16 19:03:28 -0700

    changed my about me section on my page

--- src/MiddleComponent/MiddleHeader.js
+++ src/MiddleComponent/MiddleHeader.js
@@ -16,18 +16,14 @@ const Header = () => {
         <CardTitle>About Me</CardTitle>
 
         <CardText>
-       <p> I studied computer science in college, learning starting with C# applications than learning C++, mostly learning the fundamental skills of problem solving, where the coding language is just a tool we use to solve problems. 
-</p><p>
-I have moved into learning JavaScript over the last year, and I feel very confident with the language at this point, I joined a hands on program for learning computer science in depth, I commit to doing daily pair programming and I enjoy working inside of a team structure.
-</p><p>
-Inside of this program I have learned new frameworks and skills, and ways to think about problems, I find it really good working with people with all different ways of solving problems, because it allows me to rethink how I break down and build out my own coding solutions, I feel like I am currently growing a lot in this manner.
-</p><p>
-Moving forward, I am interested in working for a company that values teamwork and communication, working for a company that would allow me to grow as a developer, with a team that pushes and guides me.
-</p><p>
-I really enjoy being a developer, and I am very excited to work with an awesome new team, talking about coding and solving problems with a team is a wonderful and growing practice.
+     <p style={{textAlign:"center"}}> My journey to becoming a software developer started while studying computer science in college. While I was introduced to C# applications and C++,  what stuck with me the most was utilizing the fundamental skills of problem solving and learning that the coding language is essential just a tool we use to solve problems.
+</p>  <p style={{textAlign:"center"}}>
+Over the past year, I've moved into learning JavaScript. After joining an immersive program at Lambda School for learning software development, I feel very confident with the language at this point. Inside of this program I have not only learned new frameworks and skills, but real-world approaches to problem-solving. One of the advantages of my program is the opportunity to work with people coming from a variety of backgrounds who have novel approaches to problem-solving. It's definitely made it easier for me to rethink, break down, and build out my own coding solutions, and I know i've grown so much as a developer as a result.
+</p><p style={{textAlign:"center"}}>
+I'm still close to my Lambda School cohort, and having committed to doing daily pair programming, I know that I greatly enjoy working inside a team structure.  Moving forward, I am interested in working for a company that values teamwork and communication, working for a company that would allow me to grow as a developer, with a team that pushes and guides me.
+</p><p style={{textAlign:"center"}}>
+I really enjoy being a developer, and I am very excited to continue to grow my skill set on a team that values teamwork, excellence, and continued growth.
 </p>
-Thank you for reading.
-
 
         </CardText>
       </CardBody>
