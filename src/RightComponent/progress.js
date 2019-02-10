df9634fd7932cfdf697039df78b5e6975b04d32a
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-09 23:25:17 -0700

    formatted this component to be more readable

--- src/RightComponent/progress.js
+++ src/RightComponent/progress.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { Label, ListGroupItem, ListGroup } from "reactstrap";
 
 const ProgressBars = () => {
@@ -7,27 +8,45 @@ const ProgressBars = () => {
     <div className="progress-styles prof-container">
       <ListGroup>
         <Label className="prof-styles">Proficiencies</Label>
+
         <div className="list-items">
           <ListGroupItem>React.js</ListGroupItem>
+
           <ListGroupItem>JavaScript</ListGroupItem>
+
           <ListGroupItem>HTML/CSS</ListGroupItem>
+
           <ListGroupItem>LESS</ListGroupItem>
+
           <ListGroupItem>SASS</ListGroupItem>
+
           <ListGroupItem>MongoDB</ListGroupItem>
+
           <ListGroupItem>Mongoose</ListGroupItem>
+
           <ListGroupItem>Node.js</ListGroupItem>
+
           <ListGroupItem>Expess.js</ListGroupItem>
+
           <ListGroupItem>Redux</ListGroupItem>
+
           <ListGroupItem>React Context</ListGroupItem>
+
           <ListGroupItem>Reactstrap/Bootstrap</ListGroupItem>
+
           <ListGroupItem>Animate.css</ListGroupItem>
+
           <ListGroupItem>Python</ListGroupItem>
+
           <ListGroupItem>SQL</ListGroupItem>
+
           <ListGroupItem>C++</ListGroupItem>
+
           <ListGroupItem>C</ListGroupItem>
         </div>
       </ListGroup>
     </div>
   );
 };
+
 export default ProgressBars;
