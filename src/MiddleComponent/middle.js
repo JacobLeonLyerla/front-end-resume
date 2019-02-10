96a1fc58734d0300bea366ad9f9b8808e9c35c28
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-09 23:24:54 -0700

    formatted this component to be more readable

--- src/MiddleComponent/middle.js
+++ src/MiddleComponent/middle.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "./MiddleHeader";
 
-
 // stateless component because nothing is set on the state.
 const MiddlePanel = () => {
   return (
     // just returns the header component, i really could have written the header stuff here, but i wanted a clean example of a component rendering another.
-  
-      <Header />
- 
+
+    <Header />
   );
 };
 export default MiddlePanel;
