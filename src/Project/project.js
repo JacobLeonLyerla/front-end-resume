522352142943e7725e8eb0b51ba2dad54c9dd43b
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-09 23:25:08 -0700

    formatted this component to be more readable

--- src/Project/project.js
+++ src/Project/project.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import axios from "axios";
 
 import {
@@ -11,42 +12,52 @@ import {
   Button,
   Col
 } from "reactstrap";
+
 import { Link } from "react-router-dom";
 
 class Note extends Component {
   constructor(props) {
+
     super(props);
     this.state = {
       project: []
     };
   }
+
   componentDidMount() {
     const { id } = this.props.match.params;
-    console.log(id);
-    console.log(id);
+
     this.getProjectById(id);
   }
+
   getProjectById = id => {
     axios
       .get(`https://jacob-lyerla-resume.herokuapp.com/projects/${id}`)
       .then(response => {
-        console.log(response);
         this.setState(() => ({ project: response.data }));
       })
       .catch(err => {
         console.log("failed to get note");
       });
   };
+
   render() {
-    console.log(this.props);
     return (
       <Col md="7" className="singleCard-styles">
-        <Card key={this.state.project._id}>
-          <CardBody>
-            <CardImg src={this.state.project.img} />
-            <CardTitle>{this.state.project.title}</CardTitle>
 
-            <CardText>{this.state.project.description}</CardText>
+        <Card key={this.state.project._id}>
+
+          <CardBody>
+
+            <CardImg src={this.state.project.img} />
+
+            <CardTitle>
+            {this.state.project.title}
+            </CardTitle>
+
+            <CardText>
+            {this.state.project.description}
+            </CardText>
 
             <CardLink href={this.state.project.github}>
               <div style={{ color: "black" }}>Links</div>:
@@ -59,11 +70,14 @@ class Note extends Component {
           </CardBody>
 
           <Link to={"/projects"} style={{ textDecoration: "none" }}>
-            <Button>Project List</Button>
+            <Button>
+              Project List
+              </Button>
           </Link>
         </Card>
       </Col>
     );
   }
 }
+
 export default Note;
