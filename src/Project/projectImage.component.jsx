e28cda5941e00de392008c47592d673c075a47bc
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-26 02:13:57 -0700

    created a component that renders our our current project image and the links to the code and site.
    Also it has the functions I passed to change what project is currently being viewed. since the functions are not bound when they are used they actually change the state in the parent component allowing us to change the current array item in both this component and our dependencies

---
+++ src/Project/projectImage.component.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import "../css/project.css"
+import { Card, Row, Col, CardBody, ListGroup, ListGroupItem } from "reactstrap";
+
+const ProjectImage = ({ project,leftArrow,rightArrow }) => {
+  const { githubFrontend, id, webpage } = project;
+  return (
+    <Col md="8" lg="8" className="progress-styles project-image">
+      
+      {/* map over our return for each object in our array */}
+      <Row>
+        <Col sm="12" md="12" lg="6" xl="12" className="card-styles">
+       
+          {/* set up a Link for the card */}
+          <Card className={`card${id}`} id="test">
+            
+            <CardBody>
+             {leftArrow()}  <div />{rightArrow()}
+            </CardBody>
+            
+          </Card>
+          <ListGroup>
+            <div className="links">
+              <a className="link-styles" target="_blank" href={webpage}>
+                <ListGroupItem>View Deployed Site</ListGroupItem>
+              </a>
+              <a className="link-styles" target="_blank" href={githubFrontend}>
+                <ListGroupItem>View The Code</ListGroupItem>
+              </a>
+            </div>
+          </ListGroup>{" "}
+        </Col>
+      </Row>
+    </Col>
+  );
+};
+
+export default ProjectImage;
