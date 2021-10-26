b1163fc43faaa8c7055959ce36f25b036c397c41
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-26 02:08:29 -0700

    created some data and stored it instead of using an database to store this data since the data I need is so small and it's really a waste to host it.

---
+++ src/Project/project.data.js
@@ -0,0 +1,20 @@
+import movieknight from "../assets/movieknight.png.gif";
+
+export const PROJECT_DATA = [
+  {
+    img: movieknight,
+    title: "Movie Knight",
+    description:
+      "Site for browsing movies currently adding the ability to log and intereact more with the site. Doing things like saving and rating movies",
+    githubFrontend: "https://github.com/JacobLeonLyerla/movieknight",
+    id: "1",
+    webpage: "https://movieknight.netlify.app/",
+    dependencies: [
+      "React",
+      "Firebase",
+      "React-Router",
+      "React Context",
+      "ReactStrap",
+    ],
+  },
+];
