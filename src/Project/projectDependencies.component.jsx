996ce8d762f5327442e4ca2bf5b314302b04dceb
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-26 02:14:26 -0700

    this is our list group that renders our the current dependencies used for that current project being previewed

---
+++ src/Project/projectDependencies.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Label,ListGroupItem,Col, ListGroup } from "reactstrap";
+const ProjectDependencies = ({ project }) => {
+  const { dependencies, id } = project;
+
+
+  return (
+    <Col md="8" lg="8"  className="progress-styles prof-container dependencies-container" >
+     <ListGroup>
+      <Label className="prof-styles">dependencies</Label>
+      <div className="list-techs list-items">
+        {
+     
+       
+          dependencies.map((dependency) => (
+              <ListGroupItem>{dependency}</ListGroupItem>
+            ))
+          
+        }
+      </div></ListGroup>
+    </Col>
+  );
+};
+
+export default ProjectDependencies;
